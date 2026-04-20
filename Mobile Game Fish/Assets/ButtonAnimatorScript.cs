68b3f41250aa52c9e6cc79a12c8cc748ca430ef7
SpaceDuck250 <alexthevlogger505@gmail.com>
2026-04-20 10:14:45 +0800

    buttonanimations

--- Mobile Game Fish/Assets/ButtonAnimatorScript.cs
+++ Mobile Game Fish/Assets/ButtonAnimatorScript.cs
@@ -1,16 +1,59 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonAnimatorScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Animator buttonAnimator;
+
+    public bool held = false;
+
+    public float sizeIncrease;
+    public Color desiredShade;
+    public float smoothValue;
+
+    public Vector3 originalScale;
+    public Color originalShade;
+
+    public RectTransform rectTransform;
+
+    public Image image;
+
+    private void Start()
     {
-        
+        originalScale = rectTransform.sizeDelta;
+        originalShade = image.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!held)
+        {
+            return;
+        }
+
+        LerpToHeldAnimation();
+    }
+
+    public void PlayHoldAnimation()
+    {
+        held = true;
+    }
+
+    public void StopHoldAnimation()
+    {
+        held = false;
+
+        rectTransform.sizeDelta = originalScale;
+    }
+
+    public void LerpToHeldAnimation()
+    {
+        Vector3 desiredScale = originalScale * sizeIncrease;
+        rectTransform.sizeDelta = Vector3.Lerp(rectTransform.sizeDelta, desiredScale, smoothValue * Time.deltaTime);
+    }
+
+    public void PlayClickedAnimation()
+    {
+        buttonAnimator.SetTrigger("clicked");
     }
 }
